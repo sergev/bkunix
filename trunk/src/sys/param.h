e236f1a418dda86a41b02dda82c547afd5f507b9
vak <vak@80b75cea-021d-0410-ac62-fcbd06c4f22d>
2006-09-16 08:01:40 +0000

    Make USR and SYS sizes in kbytes, not in 2048-byte units.

--- trunk/src/sys/param.h
+++ trunk/src/sys/param.h
@@ -15,32 +15,32 @@
 /*
  * the following 4 variables may be modified.
  */
-#define	NPROC	3	/* max number of processes */
-#define	NBLKS	500	/* 256-word blocks per diskette */
-#define	SYSSIZ	10 	/* system size in 1K words */
-#define	USRSIZ	12	/* user program size in 1K words */
-#define UCORE	(USRSIZ*32)
-#define TOPSYS	(SYSSIZ*2048)
-#define	TOPUSR	((SYSSIZ+USRSIZ)*(unsigned)2048)
-#define SWPSIZ	(USRSIZ*4+1)
+#define NPROC	3	/* max number of processes */
+#define NBLKS	500	/* 256-word blocks per diskette */
+#define SYSSIZ	20 	/* system size in 1K bytes */
+#define USRSIZ	24	/* user program size in 1K bytes */
+#define UCORE	(USRSIZ*16)
+#define TOPSYS	(SYSSIZ*1024)
+#define TOPUSR	((SYSSIZ+USRSIZ)*(unsigned)1024)
+#define SWPSIZ	(USRSIZ*2+1)
 #ifdef BGOPTION
 #define NSWAP	(NPROC*SWPSIZ+2)
 #endif
 #ifndef BGOPTION
 #define NSWAP	((NPROC-1)*SWPSIZ+1)
 #endif
-#define	SWPLO	(NBLKS-NSWAP)
+#define SWPLO	(NBLKS-NSWAP)
 
-#define	NBUF	6 		/* size of buffer cache */
-#define	NINODE	20		/* number of in core inodes */
-#define	NFILE	20		/* number of in core file structures */
-#define	NMOUNT	2		/* number of mountable file systems */
-#define	SSIZE	20		/* initial stack size (*64 bytes) */
-#define	SINCR	20		/* increment of stack (*64 bytes) */
-#define	NOFILE	15		/* max open files per process */
-#define	CANBSIZ	132		/* max size of typewriter line */
-#define	NCLIST	25		/* max total clist size */
-#define	HZ	60		/* Ticks/second of the clock */
+#define NBUF	6 		/* size of buffer cache */
+#define NINODE	20		/* number of in core inodes */
+#define NFILE	20		/* number of in core file structures */
+#define NMOUNT	2		/* number of mountable file systems */
+#define SSIZE	20		/* initial stack size (*64 bytes) */
+#define SINCR	20		/* increment of stack (*64 bytes) */
+#define NOFILE	15		/* max open files per process */
+#define CANBSIZ	132		/* max size of typewriter line */
+#define NCLIST	25		/* max total clist size */
+#define HZ	60		/* Ticks/second of the clock */
 
 /*
  * priorities
@@ -48,19 +48,19 @@
  * altered too much
  */
 #ifdef BGOPTION
-#define	PSWP	-50
+#define PSWP	-50
 #endif
 #ifndef BGOPTION
 #define PSWP	-100
 #endif
-#define	PRIBIO	-50
-#define	PSLEP	90
+#define PRIBIO	-50
+#define PSLEP	90
 
 /*
  * signals
  * dont change
  */
-#define	NSIG	20
+#define NSIG	20
 #define		SIGHUP	1	/* hangup */
 #define		SIGINT	2	/* interrupt (rubout) */
 #define		SIGQIT	3	/* quit (FS) */
@@ -80,11 +80,11 @@
  * fundamental constants
  * cannot be changed
  */
-#define	USIZE	8		/* size of user block (*64) */
-#define	NULL	0
-#define	NODEV	(-1)
-#define	ROOTINO	1		/* i number of all roots */
-#define	DIRSIZ	14		/* max characters per directory */
+#define USIZE	8		/* size of user block (*64) */
+#define NULL	0
+#define NODEV	(-1)
+#define ROOTINO	1		/* i number of all roots */
+#define DIRSIZ	14		/* max characters per directory */
 
 /*
  * configuration dependent variables
