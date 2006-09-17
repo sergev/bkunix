a8e807352a3357c423215c9383862ab3503636bd
vak <vak@80b75cea-021d-0410-ac62-fcbd06c4f22d>
2006-09-17 09:13:48 +0000

    Added syscall.h - definitions of syscall numbers.

---
+++ trunk/src/lib/libc/syscall.h
@@ -0,0 +1,32 @@
+/*
+ * System call numbers.
+ */
+#define SYS_exit	1	/* Terminate the calling process */
+#define SYS_fork	2	/* Create a new process */
+#define SYS_read	3	/* Read/write file */
+#define SYS_write	4
+#define SYS_open	5	/* Open or create a file for reading or writing */
+#define SYS_close	6	/* Close a file */
+#define SYS_wait	7	/* Get a status of child process */
+#define SYS_creat	8	/* Create a new file */
+#define SYS_link	9	/* Make a file link */
+#define SYS_unlink	10	/* Remove directory entry */
+#define SYS_exec	11	/* Execute a file */
+#define SYS_chdir	12	/* Change current working directory */
+#define SYS_time	13	/* Get time of day */
+#define SYS_mknod	14	/* Make a special file node */
+#define SYS_chmod	15	/* Change mode of file */
+#define SYS_break	17	/* Increase data segment size */
+#define SYS_stat	18	/* Get file status by file name */
+#define SYS_seek	19	/* Reposition read/write file offset */
+#define SYS_getpid	20	/* Get calling process identification */
+#define SYS_getuid	24	/* Get user identification */
+#define SYS_stime	25	/* Set time of day */
+#define SYS_alarm	27	/* Get/set value of process alarm timer */
+#define SYS_fstat	28	/* Get file status by file descriptor */
+#define SYS_pause	29	/* Stop until signal */
+#define SYS_stty	31	/* Set and get terminal state */
+#define SYS_gtty	32
+#define SYS_sync	36	/* Force a write of modified buffers out to disk */
+#define SYS_dup		41	/* Duplicate an existing file descriptor */
+#define SYS_sig		48	/* Set signal handler */
--- trunk/src/lib/libc/unistd.h
+++ trunk/src/lib/libc/unistd.h
@@ -1,146 +1,181 @@
 /*
  * Terminate the calling process.
  */
-void exit(int);					/* 1 = exit */
+void exit(int);
 
 /*
  * Create a new process.
  */
-int fork(void);					/* 2 = fork */
+int fork(void);
 
 /*
  * Read/write file.
  */
-int read(int, char*, int);			/* 3 = read */
-int write(int, char*, int);			/* 4 = write */
+int read(int, char*, int);
+int write(int, char*, int);
 
 /*
  * Open or create a file for reading or writing.
  */
-int open(char*, int);				/* 5 = open */
+int open(char*, int);
 
 /*
  * Close a file.
  */
-int close(int);					/* 6 = close */
+int close(int);
 
 /*
  * Get a status of child process.
  */
-int wait(int*);					/* 7 = wait */
+int wait(int*);
 
 /*
  * Create a new file.
  */
-int creat(char*, int);				/* 8 = creat */
+int creat(char*, int);
 
 /*
  * Make a file link.
  */
-int link(char*, char*);				/* 9 = link */
+int link(char*, char*);
 
 /*
  * Remove directory entry.
  */
-int unlink(char*);				/* 10 = unlink */
+int unlink(char*);
 
 /*
  * Execute a file.
  */
-int execv(char*, char**);			/* 11 = exec */
+int execv(char*, char**);
 
 /*
  * Change current working directory.
  */
-int chdir(char*);				/* 12 = chdir */
+int chdir(char*);
 
 /*
  * Get time of day.
  */
-void time(int*);				/* 13 = time */
+void time(int*);
 
 /*
  * Make a special file node.
  */
-int mknod(char*, int, int);			/* 14 = mknod */
+int mknod(char*, int, int);
 
 /*
  * Change mode of file.
  */
-int chmod(char*, int);				/* 15 = chmod */
+int chmod(char*, int);
 
 /*
  * Increase data segment size.
  */
-char *sbrk(int);				/* 17 = break */
+char *sbrk(int);
 
 /*
  * Get file status by file name.
  */
-int stat(char*, int*);				/* 18 = stat */
+int stat(char*, int*);
 
 /*
  * Reposition read/write file offset.
  */
-int seek(int, unsigned int, int);		/* 19 = seek */
+int seek(int, unsigned int, int);
 
 /*
  * Get calling process identification.
  */
-int getpid(void);				/* 20 = getpid */
+int getpid(void);
 
 /*
  * Get user identification.
  */
-int getuid(void);				/* 24 = getuid */
+int getuid(void);
 
 /*
  * Set time of day.
  */
-int stime(int*);				/* 25 = stime */
+int stime(int*);
 
 /*
  * Get/set value of process alarm timer.
  */
-int alarm(int);					/* 27 = alarm */
+int alarm(int);
 
 /*
  * Get file status by file descriptor.
  */
-int fstat(int, int*);				/* 28 = fstat */
+int fstat(int, int*);
 
 /*
  * Stop until signal.
  */
-void pause(void);				/* 29 = pause */
+void pause(void);
 
 /*
  * Set and get terminal state.
  */
-int stty(int, int*);				/* 31 = stty */
-int gtty(int, int*);				/* 32 = gtty */
+int stty(int, int*);
+int gtty(int, int*);
 
 /*
  * Force a write of modified buffers out to disk.
  */
-void sync(void);				/* 36 = sync */
-
-/*
- * Terminate a background process.
- */
-void kill(void);				/* 37 = kill */
+void sync(void);
 
 /*
  * Duplicate an existing file descriptor.
  */
-int dup(int);					/* 41 = dup */
+int dup(int);
 
 /*
  * Set signal handler.
  */
-int signal(int, int);				/* 48 = sig */
+int signal(int, int);
+
+#if 0
+/*
+ * Empty syscalls:
+ */
+	0, nullsys,			/*  0 = indir */
+	2, nullsys,			/* 16 = chown */
+	0, nullsys,			/* 23 = setuid */
+	3, nullsys,			/* 26 = ptrace */
+	1, nullsys,			/* 30 = smdate; inoperative */
+	0, nullsys,			/* 34 = nice */
+	1, nullsys,			/* 43 = times */
+	0, nullsys,			/* 46 = setgid */
+	0, nullsys,			/* 47 = getgid */
 
 /*
- * Become a background process.
+ * Unavalable syscalls:
  */
-void bground(void);				/* 63 = bground */
+	3, nosys,			/* 21 = mount */
+	1, nosys,			/* 22 = umount */
+	0, nosys,			/* 33 = x */
+	0, nosys,			/* 35 = sleep */
+	1, nosys,			/* 37 = kill */
+	0, nosys,			/* 38 = switch */
+	0, nosys,			/* 39 = x */
+	0, nosys,			/* 40 = x */
+	0, nosys,			/* 42 = pipe */
+	4, nosys,			/* 44 = prof */
+	0, nosys,			/* 45 = tiu */
+	0, nosys,			/* 49 = x */
+	0, nosys,			/* 50 = x */
+	0, nosys,			/* 51 = x */
+	0, nosys,			/* 52 = x */
+	0, nosys,			/* 53 = x */
+	0, nosys,			/* 54 = x */
+	0, nosys,			/* 55 = x */
+	0, nosys,			/* 56 = x */
+	0, nosys,			/* 57 = x */
+	0, nosys,			/* 58 = x */
+	0, nosys,			/* 59 = x */
+	0, nosys,			/* 60 = x */
+	0, nosys,			/* 61 = x */
+	0, nosys,			/* 62 = x */
+	0, nosys,			/* 63 = x */
+#endif
