a94cee967bf8e1ddbc33bdf4d97ff12914da0007
vak <vak@80b75cea-021d-0410-ac62-fcbd06c4f22d>
2006-09-23 19:33:18 +0000

    Added ar.h file.

---
+++ trunk/cross-pcc/ld/ar.h
@@ -0,0 +1,12 @@
+#define	ARCMAGIC 0177545
+
+struct ar_hdr {
+	char ar_name[14];		/* name */
+	long ar_date;			/* modification time */
+	char ar_uid;			/* user id */
+	char ar_gid;			/* group id */
+	int ar_mode;			/* octal file permissions */
+	long ar_size;			/* size in bytes */
+} archdr;
+
+#define AR_HDRSIZE	(14 + 4 + 1 + 1 + 2 + 4)
