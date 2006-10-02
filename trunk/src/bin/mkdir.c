18187623352d1ad5d8e31d057d6f5e7f5d6db644
vak <vak@80b75cea-021d-0410-ac62-fcbd06c4f22d>
2006-10-02 14:08:28 +0000

    Mkdir ported from 2.11BSD. Added signal.h. Added library finctions for unimplemented syscalls, returning 0 or -1 as appropriate.

--- trunk/src/bin/mkdir.c
+++ trunk/src/bin/mkdir.c
@@ -1,17 +1,59 @@
 /*
-** make directory
-*/
+ * make directory
+ */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/stat.h>
 
-#include	<signal.h>
-#include	<stdio.h>
-
-int	Errors = 0;
-char	*strcat();
-char	*strcpy();
-
-main(argc, argv)
-char *argv[];
+int
+makedir(d)
+	char *d;
 {
+	char pname[128], dname[128];
+	struct stat st;
+	register int i, slash = 0;
+
+	pname[0] = '\0';
+	for (i = 0; d[i]; ++i)
+		if(d[i] == '/')
+			slash = i + 1;
+	if (slash)
+		strncpy(pname, d, slash);
+	strcpy(pname+slash, ".");
+	if (stat(pname, &st) < 0) {
+		printf("mkdir: cannot access %s\n", pname);
+		return 1;
+	}
+	if (mknod(d, 040777, 0) < 0) {
+		printf("mkdir: cannot make directory %s\n", d);
+		return 1;
+	}
+	chown(d, getuid(), getgid());
+	strcpy(dname, d);
+	strcat(dname, "/.");
+	if (link(d, dname) < 0) {
+		printf("mkdir: cannot link %s\n", dname);
+		unlink(d);
+		return 1;
+	}
+	strcat(dname, ".");
+	if (link(pname, dname) < 0) {
+		printf("mkdir: cannot link %s\n", dname);
+		dname[strlen(dname)] = '\0';
+		unlink(dname);
+		unlink(d);
+		return 1;
+	}
+	return 0;
+}
+
+int
+main(argc, argv)
+	char *argv[];
+{
+	int nerrors;
 
 	signal(SIGHUP, SIG_IGN);
 	signal(SIGINT, SIG_IGN);
@@ -19,53 +61,12 @@ char *argv[];
 	signal(SIGPIPE, SIG_IGN);
 	signal(SIGTERM, SIG_IGN);
 
-	if(argc < 2) {
-		fprintf(stderr, "mkdir: arg count\n");
-		exit(1);
+	if (argc < 2) {
+		printf("Usage: mkdir dirname...\n");
+		return 1;
 	}
-	while(--argc)
-		mkdir(*++argv);
-	exit(Errors!=0);
-}
-
-mkdir(d)
-char *d;
-{
-	char pname[128], dname[128];
-	register i, slash = 0;
-
-	pname[0] = '\0';
-	for(i = 0; d[i]; ++i)
-		if(d[i] == '/')
-			slash = i + 1;
-	if(slash)
-		strncpy(pname, d, slash);
-	strcpy(pname+slash, ".");
-	if (access(pname, 02)) {
-		fprintf(stderr,"mkdir: cannot access %s\n", pname);
-		++Errors;
-		return;
-	}
-	if ((mknod(d, 040777, 0)) < 0) {
-		fprintf(stderr,"mkdir: cannot make directory %s\n", d);
-		++Errors;
-		return;
-	}
-	chown(d, getuid(), getgid());
-	strcpy(dname, d);
-	strcat(dname, "/.");
-	if((link(d, dname)) < 0) {
-		fprintf(stderr, "mkdir: cannot link %s\n", dname);
-		unlink(d);
-		++Errors;
-		return;
-	}
-	strcat(dname, ".");
-	if((link(pname, dname)) < 0) {
-		fprintf(stderr, "mkdir: cannot link %s\n",dname);
-		dname[strlen(dname)] = '\0';
-		unlink(dname);
-		unlink(d);
-		++Errors;
-	}
+	nerrors = 0;
+	while (--argc)
+		nerrors += makedir(*++argv);
+	return (nerrors != 0);
 }
