47d5270c76f0cd6b2070efc1265107dab57d63a6
vak <vak@80b75cea-021d-0410-ac62-fcbd06c4f22d>
2006-10-04 09:01:27 +0000

    Working wc, ln, pwd.

--- trunk/src/bin/wc.c
+++ trunk/src/bin/wc.c
@@ -1,73 +1,74 @@
-/* wc line and word count */
+/*
+ * wc - line and word count
+ */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int	buf[259];
-int wordct[2];
-int twordct[2];
-int linect[2];
-int tlinect[2];
+char buf[512+4];
+long wordct;
+long twordct;
+long linect;
+long tlinect;
 
-main(argc,argv)
-char **argv;
+void
+diag(s)
+	char *s;
 {
-	int i, token;
+	write(2, s, strlen(s));
+}
+
+int
+main(argc, argv)
+	char **argv;
+{
+	int i, token, fd;
 	register char *p1, *p2;
 	register int c;
 
 	i = 1;
 	do {
-		if(argc<=1) buf[0] = 0;
-		else if(fopen(argv[i],buf)<0) {
+		if (argc <= 1)
+			fd = 0;
+		else if ((fd = open(argv[i], 0)) < 0) {
 			diag(argv[i]);
 			diag(": cannot open\n");
 			continue;
 		}
 		p1 = 0;
 		p2 = 0;
-		linect[0] = linect[1] = 0;
-		wordct[0] = wordct[1] = 0;
+		linect = 0;
+		wordct = 0;
 		token = 0;
-		for(;;) {
-			if(p1 >= p2) {
-				p1 = &buf[1];
-				c = read(buf[0], p1, 512);
-				if(c <= 0)
+		for (;;) {
+			if (p1 >= p2) {
+				c = read(fd, buf, 512);
+				if (c <= 0)
 					break;
-				p2 = p1+c;
+				p1 = buf;
+				p2 = buf + c;
 			}
-			c = 0;
-			c =| *p1++;
-			if(' '<c&&c<0177) {
-				if(!token++) {
-					if(++wordct[1]==0)
-						wordct[0]++;
-				}
+			c = *p1++;
+			if (' ' < c && c < 0177) {
+				if (! token++)
+					wordct++;
 			} else {
-				if(c=='\n') {
-					if(++linect[1]==0)
-						linect[0]++;
-				}
-				else if(c!=' '&&c!='\t')
+				if (c == '\n')
+					linect++;
+				else if (c != ' ' && c != '\t')
 					continue;
 				token = 0;
 			}
 		}
-		printf("%7s ",locv(linect[0],linect[1]));
-		printf("%7s ",locv(wordct[0],wordct[1]));
-		printf("%s\n", argc<=1?"":argv[i]);
-		close(buf[0]);
-		ladd(tlinect, tlinect, linect);
-		ladd(twordct, twordct, wordct);
-	} while(++i<argc);
-	if(argc > 2) {
-		printf("%7s ",locv(tlinect[0],tlinect[1]));
-		printf("%7s ",locv(twordct[0],twordct[1]));
-		printf("total\n");
-	}
-}
+		printf("%7ld %7ld %s\n", linect, wordct,
+			argc <= 1 ? "" : argv[i]);
+		close(fd);
+		tlinect += linect;
+		twordct += wordct;
+	} while (++i<argc);
 
-diag(s)
-char *s;
-{
-	while(*s)
-		write(2,s++,1);
+	if (argc > 2)
+		printf("%7ld %7ld total\n", tlinect, twordct);
+	return 0;
 }
