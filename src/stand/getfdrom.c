976dbb36235dfadf0c153948048dfd3a7be53ae6
Serge Vakulenko <serge.vakulenko@gmail.com>
2006-11-14 13:57:30 +0000

    Getfdrom is working. Added fdinfo - print ioarea data.

--- src/stand/getfdrom.c
+++ src/stand/getfdrom.c
@@ -1,72 +1,43 @@
 /*
- * Write floppy controller BIOS to floppy swap area.
+ * Write floppy controller BIOS to floppy.
  */
 int bootdev;
-char ioarea [64];
 
-void fdinit ()
+int main ()
 {
-	/* When calling floppy BIOS, we must pass an address of
-	 * i/o area in R3. Declare two register variables:
-	 * the first is always placed in R4 by compiler,
-	 * the second - in R3. */
-	register char *r4, *r3;
+	register int blk, ioarea, addr;
 
-	r3 = ioarea;
-	((void(*)()) 0160010) ();
-}
-
-void fdstop ()
-{
-	/* Stop floppy motor. */
-	*(int*) 0177130 = 0;
-}
-
-void fdwrite (mem, blk)
-	char *mem;
-	int blk;
-{
-	register int r4;
-	register char *r3, *r2;
+	/* Using BIOS i/o area at address 02000. */
+	ioarea = 02000;		/* r3 */
+	addr = 0160000;		/* r2 */
+	blk = 80;		/* r4 */
+	printf ("Get floppy controller ROM.\n");
 
 	printf ("Write buffer ");
-	printhex (mem);
+	printhex (addr);
 	printf (" to device ");
 	printhex (bootdev);
 	printf (" block ");
 	printhex (blk);
 	printf ("\n");
 
-	r3 = ioarea;
-	r3[034] = bootdev;
-	r2 = mem;
-	r4 = blk;
-
-	asm("mov $-256, r1");	/* word cnt, negative for write */
+	asm("mov $-4096, r1");	/* word cnt, negative for write */
 	asm("mov r4, r0");	/* blk num */
 	asm("mov r5,-(sp)");	/* r5 will be corrupted */
 	asm("jsr pc, *$0160004");
 	asm("bcs 1f");
 	asm("mov (sp)+,r5");
-	asm("jmp cret");
+	asm("jmp 2f");
 	asm("1: mov (sp)+,r5");
 
-	printf (" -- Error.\n");
-}
+	printf (" -- Error ");
+	printhex (*(unsigned char*) 052);
+	printf ("\n");
 
-int main()
-{
-	register char *mem;
-	register int blk;
-
-	printf ("Get floppy controller ROM.\n");
-	fdinit ();
-	mem = (char*) 0160000;
-	for (blk=0; blk<8; blk++) {
-		fdwrite (mem, blk + 1600 - 128);
-		mem += 512;
-	}
+	asm("2:");
 	printf ("Done.\n");
-	fdstop ();
+
+	/* Stop floppy motor. */
+	*(int*) 0177130 = 0;
 	return 0;
 }
