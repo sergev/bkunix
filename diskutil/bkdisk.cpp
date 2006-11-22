3e5ba3db82288a0b6fe3dd22a32a73e73d375484
Serge Vakulenko <serge.vakulenko@gmail.com>
2006-11-22 17:50:36 +0000

    Bkdisk utility works fine.

--- diskutil/bkdisk.cpp
+++ diskutil/bkdisk.cpp
@@ -17,290 +17,343 @@ int DISK_TRACKS	= 80;		// tracks per side
 #define DISK_SECTORS	10	// sectors per track
 #define DISK_DATARATE	2	// 2 is double-density
 #define SECTOR_SIZE_CODE 2	// 0=128, 1=256, 2=512, 3=1024, ...
-#define SECTOR_GAP3	0x25	// gap3 size between sectors
+#define SECTOR_GAP3	0x2e	// gap3 size between sectors for format
 #define SECTOR_FILL	0xff	// fill byte for formatted sectors
 #define SECTOR_BASE	1	// first sector number on track
-#define TRACK_SKEW	0	// format skew to the same sector on the next track
 
-#define TRACK_SIZE          ((128<<SECTOR_SIZE_CODE)*DISK_SECTORS)
+#define TRACK_SIZE	((128<<SECTOR_SIZE_CODE)*DISK_SECTORS)
 
 ///////////////////////////////////////////////////////////////////////////////
 
 DWORD dwRet;
 
-bool CmdRead (HANDLE h_, BYTE cyl_, BYTE head_, BYTE start_, BYTE count_, BYTE size_, PVOID pv_)
+bool CmdRead (HANDLE h, BYTE cyl, BYTE head, BYTE start,
+	BYTE count, BYTE size, PVOID pv)
 {
-    FD_READ_WRITE_PARAMS rwp = { FD_OPTION_MFM, head_, cyl_,head_, start_,size_,start_+count_, 0x0a,0xff };
-    return !!DeviceIoControl(h_, IOCTL_FDCMD_READ_DATA, &rwp, sizeof(rwp), pv_, count_*(128<<rwp.size), &dwRet, NULL);
+	FD_READ_WRITE_PARAMS rwp = { FD_OPTION_MFM, head, cyl, head,
+		start, size, start+count, 0x0a, 0xff };
+
+	return !!DeviceIoControl(h, IOCTL_FDCMD_READ_DATA, &rwp, sizeof(rwp),
+		pv, count * (128 << rwp.size), &dwRet, NULL);
 }
 
-bool CmdWrite (HANDLE h_, BYTE cyl_, BYTE head_, BYTE start_, BYTE count_, BYTE size_, PVOID pv_)
+bool CmdWrite (HANDLE h, BYTE cyl, BYTE head, BYTE start,
+	BYTE count, BYTE size, PVOID pv)
 {
-    FD_READ_WRITE_PARAMS rwp = { FD_OPTION_MFM, head_, cyl_,head_, start_,size_,start_+count_, 0x0a,0xff };
-    return !!DeviceIoControl(h_, IOCTL_FDCMD_WRITE_DATA, &rwp, sizeof(rwp), pv_, count_*(128<<rwp.size), &dwRet, NULL);
+	FD_READ_WRITE_PARAMS rwp = { FD_OPTION_MFM, head, cyl, head,
+		start, size, start+count, 0x0a, 0xff };
+
+	return !!DeviceIoControl(h, IOCTL_FDCMD_WRITE_DATA, &rwp, sizeof(rwp),
+		pv, count * (128 << rwp.size), &dwRet, NULL);
 }
 
-bool CmdVerify (HANDLE h_, BYTE cyl_, BYTE head_, BYTE start_, BYTE end_, BYTE size_)
+bool CmdVerify (HANDLE h, BYTE cyl, BYTE head, BYTE start,
+	BYTE end, BYTE size)
 {
-    FD_READ_WRITE_PARAMS rwp = { FD_OPTION_MFM, head_, cyl_,head_, start_,size_,end_, 0x0a,0xff };
-    return !!DeviceIoControl(h_, IOCTL_FDCMD_VERIFY, &rwp, sizeof(rwp), NULL, 0, &dwRet, NULL);
+	FD_READ_WRITE_PARAMS rwp = { FD_OPTION_MFM, head, cyl, head,
+		start, size, end, 0x0a, 0xff };
+
+	return !!DeviceIoControl(h, IOCTL_FDCMD_VERIFY, &rwp, sizeof(rwp),
+		NULL, 0, &dwRet, NULL);
 }
 
-bool CmdFormat (HANDLE h_, PFD_FORMAT_PARAMS pfp_, ULONG ulSize_)
+bool CmdFormat (HANDLE h, PFD_FORMAT_PARAMS pfp, ULONG ulSize)
 {
-    return !!DeviceIoControl(h_, IOCTL_FDCMD_FORMAT_TRACK, pfp_, ulSize_, NULL, 0, &dwRet, NULL);
+	return !!DeviceIoControl(h, IOCTL_FDCMD_FORMAT_TRACK, pfp,
+		ulSize, NULL, 0, &dwRet, NULL);
 }
 
-bool SetDataRate (HANDLE h_, BYTE bDataRate_)
+bool SetDataRate (HANDLE h, BYTE bDataRate)
 {
-    return !!DeviceIoControl(h_, IOCTL_FD_SET_DATA_RATE, &bDataRate_, sizeof(bDataRate_), NULL, 0, &dwRet, NULL);
+	return !!DeviceIoControl(h, IOCTL_FD_SET_DATA_RATE, &bDataRate,
+		sizeof(bDataRate), NULL, 0, &dwRet, NULL);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-HANDLE OpenFloppy (int nDrive_)
+HANDLE OpenFloppy (int nDrive)
 {
-    char szDevice[32];
-    wsprintf(szDevice, "\\\\.\\fdraw%u", nDrive_);
+	char szDevice[32];
+	HANDLE h;
 
-    HANDLE h = CreateFile(szDevice, GENERIC_READ|GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-    return (h != INVALID_HANDLE_VALUE && SetDataRate(h, DISK_DATARATE)) ? h : NULL;
+	wsprintf(szDevice, "\\\\.\\fdraw%u", nDrive);
+	h = CreateFile(szDevice, GENERIC_READ|GENERIC_WRITE, 0,
+		NULL, OPEN_EXISTING, 0, NULL);
+	if (h == INVALID_HANDLE_VALUE)
+		return NULL;
+	if (! SetDataRate(h, DISK_DATARATE))
+		return NULL;
+	return h;
 }
 
-HANDLE OpenImage (LPCSTR pcsz_, bool fWrite_)
+HANDLE OpenImage (LPCSTR pcsz, bool fWrite)
 {
-    HANDLE h = fWrite_ ? CreateFile(pcsz_, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL)
-                       : CreateFile(pcsz_, GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+	HANDLE h;
 
-    return (h != INVALID_HANDLE_VALUE) ? h : NULL;
+	if (fWrite)
+		h = CreateFile(pcsz, GENERIC_WRITE, 0, NULL,
+			CREATE_ALWAYS, 0, NULL);
+	else
+		h = CreateFile(pcsz, GENERIC_READ, 0, NULL,
+			OPEN_EXISTING, 0, NULL);
+
+	return (h != INVALID_HANDLE_VALUE) ? h : NULL;
 }
 
-bool IsFloppy (LPCSTR pcsz_, int *pnDrive_=NULL)
+bool IsFloppy (LPCSTR pcsz, int *pnDrive=NULL)
 {
-    if (! pcsz_[0] || pcsz_[1] != ':' || pcsz_[2])
-        return false;
+	if (! pcsz[0] || pcsz[1] != ':' || pcsz[2])
+		return false;
 
-    if (pcsz_[0] >= 'a' && pcsz_[0] <= 'z') {
-	if (pnDrive_)
-	    *pnDrive_ = pcsz_[0] - 'a';
-    } else if (pcsz_[0] >= 'A' && pcsz_[0] <= 'Z') {
-	if (pnDrive_)
-	    *pnDrive_ = pcsz_[0] - 'A';
-    } else
-        return false;
+	if (pcsz[0] >= 'a' && pcsz[0] <= 'z') {
+		if (pnDrive)
+			*pnDrive = pcsz[0] - 'a';
+	} else if (pcsz[0] >= 'A' && pcsz[0] <= 'Z') {
+		if (pnDrive)
+			*pnDrive = pcsz[0] - 'A';
+	} else
+		return false;
 
-    return true;
+	return true;
 }
 
 DWORD GetDriverVersion ()
 {
-    DWORD dwVersion = 0;
-    HANDLE h = CreateFile("\\\\.\\fdrawcmd", GENERIC_READ|GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	DWORD dwVersion = 0;
+	HANDLE h;
 
-    if (h != INVALID_HANDLE_VALUE)
-    {
-        DeviceIoControl(h, IOCTL_FDRAWCMD_GET_VERSION, NULL, 0, &dwVersion, sizeof(dwVersion), &dwRet, NULL);
-        CloseHandle(h);
-    }
-
-    return dwVersion;
+	h = CreateFile("\\\\.\\fdrawcmd", GENERIC_READ|GENERIC_WRITE,
+		0, NULL, OPEN_EXISTING, 0, NULL);
+	if (h != INVALID_HANDLE_VALUE) {
+		DeviceIoControl(h, IOCTL_FDRAWCMD_GET_VERSION, NULL, 0,
+			&dwVersion, sizeof(dwVersion), &dwRet, NULL);
+		CloseHandle(h);
+	}
+	return dwVersion;
 }
 
-void WriteCon (const char* pcsz_, ...)
+void WriteCon (const char* pcsz, ...)
 {
-    char sz[1024];
+	char sz[1024];
 
-    va_list args;
-    va_start(args, pcsz_);
-    wvsprintf(sz, pcsz_, args);
-    va_end(args);
+	va_list args;
+	va_start(args, pcsz);
+	wvsprintf(sz, pcsz, args);
+	va_end(args);
 
-    WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), sz, lstrlen(sz), &dwRet, NULL);
+	WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), sz, lstrlen(sz),
+		&dwRet, NULL);
 }
 
 const char* LastError ()
 {
-    static char sz[256];
+	static char sz[256];
 
-    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(),
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, GetLastError(),
                 MAKELANGID(LANG_NEUTRAL, SUBLANG_SYS_DEFAULT),
 		sz, sizeof(sz), NULL);
-    CharToOem(sz, sz);
-    return GetLastError() ? sz : "no error";
+	CharToOem(sz, sz);
+	return GetLastError() ? sz : "no error";
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-bool ReadTrack (HANDLE h_, BYTE cyl_, BYTE head_, PBYTE pb_)
+bool ReadTrack (HANDLE h, BYTE cyl, BYTE head, PBYTE pb)
 {
-    return CmdRead(h_, cyl_, head_, SECTOR_BASE, DISK_SECTORS, SECTOR_SIZE_CODE, pb_);
+	return CmdRead(h, cyl, head, SECTOR_BASE, DISK_SECTORS,
+		SECTOR_SIZE_CODE, pb);
 }
 
-bool WriteTrack (HANDLE h_, BYTE cyl_, BYTE head_, PBYTE pb_)
+bool WriteTrack (HANDLE h, BYTE cyl, BYTE head, PBYTE pb)
 {
-    return CmdWrite(h_, cyl_, head_, SECTOR_BASE, DISK_SECTORS, SECTOR_SIZE_CODE, pb_);
+	WriteCon("\rWriting    track %u/%u side %u", cyl, DISK_TRACKS, head);
+	return CmdWrite(h, cyl, head, SECTOR_BASE, DISK_SECTORS,
+		SECTOR_SIZE_CODE, pb);
 }
 
-bool VerifyTrack (HANDLE h_, BYTE cyl_, BYTE head_)
+bool VerifyTrack (HANDLE h, BYTE cyl, BYTE head)
 {
-    return CmdVerify(h_, cyl_, head_, SECTOR_BASE, SECTOR_BASE+DISK_SECTORS-1, SECTOR_SIZE_CODE);
+	WriteCon("\rVerifying  track %u/%u side %u", cyl, DISK_TRACKS, head);
+	return CmdVerify(h, cyl, head, SECTOR_BASE,
+		SECTOR_BASE + DISK_SECTORS - 1, SECTOR_SIZE_CODE);
 }
 
-bool FormatTrack (HANDLE h_, BYTE cyl_, BYTE head_)
+bool FormatTrack (HANDLE h, BYTE cyl, BYTE head)
 {
-    BYTE abFormat[sizeof(FD_FORMAT_PARAMS) + sizeof(FD_ID_HEADER)*DISK_SECTORS];
+	BYTE abFormat [sizeof(FD_FORMAT_PARAMS) + sizeof(FD_ID_HEADER)*DISK_SECTORS];
 
-    PFD_FORMAT_PARAMS pfp = (PFD_FORMAT_PARAMS)abFormat;
-    pfp->flags = FD_OPTION_MFM;
-    pfp->phead = head_;
-    pfp->size = SECTOR_SIZE_CODE;
-    pfp->sectors = DISK_SECTORS;
-    pfp->gap = SECTOR_GAP3;
-    pfp->fill = SECTOR_FILL;
+	PFD_FORMAT_PARAMS pfp = (PFD_FORMAT_PARAMS)abFormat;
+	pfp->flags = FD_OPTION_MFM;
+	pfp->phead = head;
+	pfp->size = SECTOR_SIZE_CODE;
+	pfp->sectors = DISK_SECTORS;
+	pfp->gap = SECTOR_GAP3;
+	pfp->fill = SECTOR_FILL;
 
-    PFD_ID_HEADER ph = pfp->Header;
+	PFD_ID_HEADER ph = pfp->Header;
 
-    for (BYTE s = 0 ; s < pfp->sectors ; s++, ph++)
-    {
-        ph->cyl = cyl_;
-        ph->head = head_;
-        ph->sector = SECTOR_BASE + ((s + cyl_*(pfp->sectors - TRACK_SKEW)) % pfp->sectors);
-        ph->size = pfp->size;
-    }
-
-    return CmdFormat(h_, pfp, (PBYTE)ph - abFormat);
+	for (BYTE s = 0; s < pfp->sectors; s++, ph++) {
+		ph->cyl = cyl;
+		ph->head = head;
+		ph->sector = SECTOR_BASE + (s % DISK_SECTORS);
+		ph->size = pfp->size;
+	}
+	WriteCon("\rFormatting track %u/%u side %u", cyl, DISK_TRACKS, head);
+	return CmdFormat(h, pfp, (PBYTE)ph - abFormat);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 void Usage ()
 {
-    WriteCon("BK-0010 Disk Utility by Serge Vakulenko <vak@cronyx.ru>\n"
-             "Usage:\n"
-             "\tbkdisk A: image.bkd [-81]\n"
-             "\tbkdisk image.bkd A: [-81] [-f] [-v]\n"
-             "\tbkdisk --format A: [-81] [-v]\n"
-             "\tbkdisk --verify A: [-81]\n"
-             "Options:\n"
-             "\t-81\t81 tracks on disk (default 80 tracks)\n"
-             "\t-f\tdo not format before write\n"
-             "\t-v\tdo not verify after write\n"
-    );
-
-    exit(EXIT_FAILURE);
+	WriteCon("BK-0010 Disk Utility by Serge Vakulenko <vak@cronyx.ru>\n"
+		"Based on driver fdrawcmd.sys from http://simonowen.com/fdrawcmd/\n"
+		"Usage:\n"
+		"\tbkdisk A: image.bkd [-81]\n"
+		"\tbkdisk image.bkd A: [-81] [-f] [-v]\n"
+		"\tbkdisk --format A: [-81] [-v]\n"
+		"\tbkdisk --verify A: [-81]\n"
+		"Options:\n"
+		"\t-81\t81 tracks on disk (default 80 tracks)\n"
+		"\t-f\tdo not format before write\n"
+		"\t-v\tdo not verify after write\n"
+	);
+	exit(EXIT_FAILURE);
 }
 
-int main (int argc_, char *argv_[])
+enum {
+	cmdNone=0, cmdRead, cmdWrite, cmdFormat, cmdVerify
+};
+
+bool FormatFloppy (HANDLE h, HANDLE hfile,
+	int nCommand, bool fVerify, bool fFormat)
 {
-    enum { cmdNone=0, cmdRead, cmdWrite, cmdFormat, cmdVerify };
-    int nCommand = cmdNone, nDrive = -1;
-    bool fFormat = true, fVerify = true;
-    LPCSTR pcszFile = NULL;
-    HANDLE h, hfile = 0;
+	PBYTE pbTrack = (PBYTE)VirtualAlloc(NULL, TRACK_SIZE,
+		MEM_COMMIT, PAGE_READWRITE);
 
-    for (int i = 1 ; i < argc_ ; i++)
-    {
-        if (!lstrcmpi(argv_[i], "--format") && nCommand == cmdNone)
-            nCommand = cmdFormat;
-        else if (!lstrcmpi(argv_[i], "--verify") && nCommand == cmdNone)
-            nCommand = cmdVerify;
-        else if (!lstrcmpi(argv_[i], "-81"))
-            DISK_TRACKS = 81;
-        else if (!lstrcmpi(argv_[i], "-f"))
-            fFormat = false;
-        else if (!lstrcmpi(argv_[i], "-v"))
-            fVerify = false;
-        else if (argv_[i][0] == '/')
-            Usage();
-        else if (nDrive == -1 && IsFloppy(argv_[i], &nDrive) && nCommand == cmdNone)
-            nCommand = pcszFile ? cmdWrite : cmdRead;
-        else if (!pcszFile && !IsFloppy(argv_[i]) && (pcszFile = argv_[i]) && nCommand == cmdNone)
-            nCommand = (nDrive == -1) ? cmdWrite : cmdRead;
-    }
+	for (BYTE cyl = 0; cyl < DISK_TRACKS; cyl++) {
+		for (BYTE head = 0; head < DISK_SIDES; head++) {
+			if (! DeviceIoControl(h, IOCTL_FDCMD_SEEK,
+			    &cyl, sizeof(cyl), NULL, 0, &dwRet, NULL))
+				return false;
 
-    if (nCommand == cmdNone || nDrive == -1)    // require a command and a drive
-        Usage();
-    else if (!pcszFile && (nCommand == cmdRead || nCommand == cmdWrite))    // read/write require image file
-        Usage();
+			switch (nCommand) {
+			case cmdFormat:
+				if (FormatTrack(h, cyl, head) &&
+				    (! fVerify || VerifyTrack(h, cyl, head)))
+					break;
 
-    DWORD dwVersion = GetDriverVersion();
+				WriteCon("\n\nFormat failed: %s\n", LastError());
+				return false;
 
-    if (!dwVersion)
-        WriteCon("fdrawcmd.sys is not installed, see: http://simonowen.com/fdrawcmd/\n");
-    else if (HIWORD(dwVersion) != HIWORD(FDRAWCMD_VERSION))
-        WriteCon("The installed fdrawcmd.sys is not compatible with this utility.\n");
-    else if (!(h = OpenFloppy(nDrive)))
-        WriteCon("Failed to open floppy: %s\n", LastError());
-    else if (!DeviceIoControl(h, IOCTL_FD_RESET, NULL, 0, NULL, 0, &dwRet, NULL))
-        WriteCon("Failed to initialise controller: %s\n", LastError());
-    else if (pcszFile && !(hfile = OpenImage(pcszFile, (nCommand == cmdRead))))
-        WriteCon("Failed to open image: %s\n", LastError());
-    else if (nCommand == cmdWrite && GetFileSize(hfile, NULL) % TRACK_SIZE != 0)
-        WriteCon("Image file is wrong size (should be %lu bytes)\n", TRACK_SIZE*DISK_TRACKS*DISK_SIDES);
-    else
-    {
-        PBYTE pbTrack = (PBYTE)VirtualAlloc(NULL, TRACK_SIZE, MEM_COMMIT, PAGE_READWRITE);
+			case cmdRead:
+				WriteCon("\rReading track %u/%u side %u",
+					cyl, DISK_TRACKS, head);
+				if (ReadTrack(h, cyl, head, pbTrack) &&
+				    WriteFile(hfile, pbTrack,
+				    TRACK_SIZE, &dwRet, NULL))
+					break;
 
-        for (BYTE cyl = 0 ; cyl < DISK_TRACKS ; cyl++)
-        {
-            for (BYTE head = 0 ; head < DISK_SIDES ; head++)
-            {
-                if (!DeviceIoControl(h, IOCTL_FDCMD_SEEK, &cyl, sizeof(cyl), NULL, 0, &dwRet, NULL))
-                    return false;
+				WriteCon("\n\nRead failed: %s\n", LastError());
+				return false;
 
-                switch (nCommand)
-                {
-                    case cmdFormat:
-                        WriteCon("\rFormatting track %u/%u side %u", cyl, DISK_TRACKS, head);
-                        if (FormatTrack(h, cyl, head) && (!fVerify || VerifyTrack(h, cyl, head)))
-                            break;
+			case cmdWrite:
+				if (ReadFile(hfile, pbTrack, TRACK_SIZE, &dwRet, NULL) &&
+				    (! fFormat || FormatTrack(h, cyl, head)) &&
+				     WriteTrack(h, cyl, head, pbTrack) &&
+				    (! fVerify || VerifyTrack(h, cyl, head)))
+					break;
 
-                        WriteCon("\n\nFormat failed: %s\n", LastError());
-                        return EXIT_FAILURE;
+				WriteCon("\n\nWrite failed: %s\n", LastError());
+				return false;
 
-                    case cmdRead:
-                        WriteCon("\rReading track %u/%u side %u", cyl, DISK_TRACKS, head);
-                        if (ReadTrack(h, cyl, head, pbTrack) && WriteFile(hfile, pbTrack, TRACK_SIZE, &dwRet, NULL))
-                            break;
+			case cmdVerify:
+				if (VerifyTrack(h, cyl, head))
+					break;
 
-                        WriteCon("\n\nRead failed: %s\n", LastError());
-                        return EXIT_FAILURE;
+				WriteCon("\n\nVerify failed: %s\n", LastError());
+				return false;
+			}
+		}
+	}
 
-                    case cmdWrite:
-                        WriteCon("\rWriting track %u/%u side %u", cyl, DISK_TRACKS, head);
-                        if (ReadFile(hfile, pbTrack, TRACK_SIZE, &dwRet, NULL) &&
-                            (!fFormat || FormatTrack(h, cyl, head)) &&
-                             WriteTrack(h, cyl, head, pbTrack) &&
-                            (!fVerify || VerifyTrack(h, cyl, head)))
-                            break;
+	if (pbTrack)
+		VirtualFree(pbTrack, 0, MEM_RELEASE);
 
-                        WriteCon("\n\nWrite failed: %s\n", LastError());
-                        return EXIT_FAILURE;
+	switch (nCommand) {
+	case cmdFormat:
+		WriteCon("\rFormat complete.             \n");
+		break;
+	case cmdRead:
+		WriteCon("\rImage created successfully.  \n");
+		break;
+	case cmdWrite:
+		WriteCon("\rImage written successfully.  \n");
+		break;
+	case cmdVerify:
+		WriteCon("\rDisk verified successfully.  \n");
+		break;
+	}
+	return true;
+}
 
-                    case cmdVerify:
-                        WriteCon("\rVerifying track %u/%u side %u", cyl, DISK_TRACKS, head);
-                        if (VerifyTrack(h, cyl, head))
-                            break;
+int main (int argc, char *argv[])
+{
+	int nCommand = cmdNone, nDrive = -1;
+	bool fFormat = true, fVerify = true;
+	LPCSTR pcszFile = NULL;
+	HANDLE h, hfile = 0;
 
-                        WriteCon("\n\nVerify failed: %s\n", LastError());
-                        return EXIT_FAILURE;
-                }
-            }
-        }
+	for (int i = 1; i < argc; i++) {
+		if (! lstrcmpi(argv[i], "--format") && nCommand == cmdNone)
+			nCommand = cmdFormat;
+		else if (! lstrcmpi(argv[i], "--verify") &&
+		    nCommand == cmdNone)
+			nCommand = cmdVerify;
+		else if (! lstrcmpi(argv[i], "-81"))
+			DISK_TRACKS = 81;
+		else if (! lstrcmpi(argv[i], "-f"))
+			fFormat = false;
+		else if (! lstrcmpi(argv[i], "-v"))
+			fVerify = false;
+		else if (argv[i][0] == '/')
+			Usage();
+		else if (nDrive == -1 && IsFloppy(argv[i], &nDrive) &&
+		    nCommand == cmdNone)
+			nCommand = pcszFile ? cmdWrite : cmdRead;
+		else if (! pcszFile && ! IsFloppy(argv[i]) &&
+		    (pcszFile = argv[i]) && nCommand == cmdNone)
+			nCommand = (nDrive == -1) ? cmdWrite : cmdRead;
+	}
 
-        if (pbTrack)
-            VirtualFree(pbTrack, 0, MEM_RELEASE);
+	if (nCommand == cmdNone || nDrive == -1) {
+		// require a command and a drive
+		Usage();
+	} else if (!pcszFile && (nCommand == cmdRead || nCommand == cmdWrite)) {
+		// read/write require image file
+		Usage();
+	}
+	DWORD dwVersion = GetDriverVersion();
 
-        switch (nCommand)
-        {
-            case cmdFormat: WriteCon("\rFormat complete.           \n");    break;
-            case cmdRead:   WriteCon("\rImage created successfully.\n");    break;
-            case cmdWrite:  WriteCon("\rImage written successfully.\n");    break;
-            case cmdVerify: WriteCon("\rDisk verified successfully.\n");    break;
-        }
+	if (!dwVersion)
+		WriteCon("fdrawcmd.sys is not installed, see: http://simonowen.com/fdrawcmd/\n");
+	else if (HIWORD(dwVersion) != HIWORD(FDRAWCMD_VERSION))
+		WriteCon("The installed fdrawcmd.sys is not compatible with this utility.\n");
+	else if (! (h = OpenFloppy(nDrive)))
+		WriteCon("Failed to open floppy: %s\n", LastError());
+	else if (! DeviceIoControl(h, IOCTL_FD_RESET, NULL, 0, NULL,
+	    0, &dwRet, NULL))
+		WriteCon("Failed to initialise controller: %s\n", LastError());
+	else if (pcszFile &&
+	    ! (hfile = OpenImage(pcszFile, (nCommand == cmdRead))))
+		WriteCon("Failed to open image: %s\n", LastError());
+	else if (nCommand == cmdWrite &&
+	    GetFileSize(hfile, NULL) % TRACK_SIZE != 0)
+		WriteCon("Image file is wrong size (should be multiple of %lu bytes)\n",
+			TRACK_SIZE);
+	else if (FormatFloppy (h, hfile, nCommand, fVerify, fFormat))
+		return EXIT_SUCCESS;
 
-        return EXIT_SUCCESS;
-    }
-
-    return EXIT_FAILURE;
+	return EXIT_FAILURE;
 }
